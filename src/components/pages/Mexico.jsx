43349c915b0186569273e2bf38fa1f7d3b814cdb
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-04-04 20:13:09 -0700

    new mexico video

--- src/components/pages/Mexico.jsx
+++ src/components/pages/Mexico.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+/* eslint-disable react/no-unknown-property */
 import { Heading, Flex, Box, Stack } from "@chakra-ui/react";
 const Mexico = () => {
   return (
     <Box p={[8, 8, 24]} align="center">
       <Heading as="h2" size="2xl" marginBottom="1rem">
-        Learn more about Meredith’s life in Mexico (full time since May 2020) in
-        the videos below:
+        Learn more about Meredith’s life in Mexico
+      </Heading>
+      <Heading as="h3" size="1xl" marginBottom="1rem">
+        (full time since May 2020) in the videos below
       </Heading>
       <Stack spacing={[4, 4, 8]}>
+        <Heading as="h4" size="md">
+          Meredith&apos;s tour to the Saint Germaine vortex
+        </Heading>
+        <Flex justifyContent="center" alignItems="center">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/8hAkVFZ4R_A?si=6Op2o7niNMa_Ux7u"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </Flex>
         <Heading as="h4" size="md">
           A 45 minute interview with Meredith by Bill Dallas-Lewis about life in
           Ajijic, Mexico
